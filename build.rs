7bf2cbb009c3cdd17544343c5a1e43d43c25bcd3
Coldstuff1 <samuel.faubert@mail.mcgill.ca>
2024-03-03 16:16:13 -0500

    async now working

--- build.rs
+++ build.rs
@@ -1,14 +1,7 @@
-use protobuf_codegen::{Codegen, Customize};
+use std::error::Error;
 
-fn main() {
-    Codegen::new()
-        .protoc()
-        .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
-        .customize(Customize::default().tokio_bytes(true))
-        .includes(&["src/protos"])
-        .input("src/protos/client.proto")
-        .cargo_out_dir("protos")
-        .run_from_script();
+fn main() -> Result<(), Box<dyn Error>> {
+    tonic_build::compile_protos("protos/client.proto")?;
 
-    println!("built!");
+    Ok(())
 }
